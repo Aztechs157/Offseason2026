34c14853965c1b2f73cc7ed95469dec716123d93
Programming Laptop 2 <aztechs157General@gmail.com>
2026-05-05 18:27:44 -0400

    build to make code work - RN

--- src/main/java/org/team157/robot/BuildConstants.java
+++ src/main/java/org/team157/robot/BuildConstants.java
@@ -5,13 +5,13 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "Offseason2026-1";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 4;
-  public static final String GIT_SHA = "2c6f92a16e96561aef93aabcae4b94d8e154f158";
-  public static final String GIT_DATE = "2026-05-01 17:59:37 EDT";
+  public static final int GIT_REVISION = 5;
+  public static final String GIT_SHA = "6a5b987cdc7f26ab758d12dc55855c4c9f44552f";
+  public static final String GIT_DATE = "2026-05-01 20:07:40 EDT";
   public static final String GIT_BRANCH = "uptakeRollerSubsystem";
-  public static final String BUILD_DATE = "2026-05-01 19:49:36 EDT";
-  public static final long BUILD_UNIX_TIME = 1777679376769L;
-  public static final int DIRTY = 1;
+  public static final String BUILD_DATE = "2026-05-05 18:13:05 EDT";
+  public static final long BUILD_UNIX_TIME = 1778019185474L;
+  public static final int DIRTY = 0;
 
   private BuildConstants() {}
 }
