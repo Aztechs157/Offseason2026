67949ca15b5785e9cd8220475b03201784aa7934
Programming Laptop 2 <aztechs157General@gmail.com>
2026-05-05 18:58:13 -0400

    Fix file structure, from java/frc/robot to java/team157/org/robot - RN

---
+++ src/main/java/org/team157/robot/BuildConstants.java
@@ -0,0 +1,17 @@
+package org.team157.robot;
+
+/** Automatically generated file containing build version information. */
+public final class BuildConstants {
+  public static final String MAVEN_GROUP = "";
+  public static final String MAVEN_NAME = "Offseason2026-1";
+  public static final String VERSION = "unspecified";
+  public static final int GIT_REVISION = 5;
+  public static final String GIT_SHA = "2d2c54529aab40ce71881783572279dd5947a06c";
+  public static final String GIT_DATE = "2026-05-05 18:30:03 EDT";
+  public static final String GIT_BRANCH = "dev";
+  public static final String BUILD_DATE = "2026-05-05 18:55:00 EDT";
+  public static final long BUILD_UNIX_TIME = 1778021700018L;
+  public static final int DIRTY = 0;
+
+  private BuildConstants() {}
+}
