2bf7b380e446a76b0692467a6b6d2356ad89f3b6
Programming Laptop 2 <aztechs157General@gmail.com>
2026-06-05 18:17:46 -0400

    bring over template vision from Akit, defucklate gradle, install photonlib

---
+++ src/main/java/frc/robot/BuildConstants.java
@@ -0,0 +1,17 @@
+package frc.robot;
+
+/** Automatically generated file containing build version information. */
+public final class BuildConstants {
+  public static final String MAVEN_GROUP = "";
+  public static final String MAVEN_NAME = "Offseason2026-1";
+  public static final String VERSION = "unspecified";
+  public static final int GIT_REVISION = 6;
+  public static final String GIT_SHA = "67949ca15b5785e9cd8220475b03201784aa7934";
+  public static final String GIT_DATE = "2026-05-05 18:58:13 EDT";
+  public static final String GIT_BRANCH = "AkitVisionTest";
+  public static final String BUILD_DATE = "2026-06-05 18:13:57 EDT";
+  public static final long BUILD_UNIX_TIME = 1780697637777L;
+  public static final int DIRTY = 1;
+
+  private BuildConstants() {}
+}
