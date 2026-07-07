83e1cd48fdc11067de53b07d5af6e55672cbbccb
Programming Laptop 2 <aztechs157General@gmail.com>
2026-07-06 20:20:20 -0400

    added and configed the sandstorm model (on this branch 1st because I dont trust that dev is up to date on akit/ascope) - RN

--- src/main/java/org/team157/robot/BuildConstants.java
+++ src/main/java/org/team157/robot/BuildConstants.java
@@ -5,12 +5,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "Offseason2026-1";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 5;
-  public static final String GIT_SHA = "1de1afb3b3fc920bd7bcd5e77024775ebb771583";
-  public static final String GIT_DATE = "2026-05-01 19:42:59 EDT";
+  public static final int GIT_REVISION = 6;
+  public static final String GIT_SHA = "d5ab43675aaaa8cceae69b41a7e2d10e839c4624";
+  public static final String GIT_DATE = "2026-05-26 15:18:42 EDT";
   public static final String GIT_BRANCH = "shooterFlywheel";
-  public static final String BUILD_DATE = "2026-05-26 15:17:58 EDT";
-  public static final long BUILD_UNIX_TIME = 1779823078842L;
+  public static final String BUILD_DATE = "2026-07-06 19:35:32 EDT";
+  public static final long BUILD_UNIX_TIME = 1783380932228L;
   public static final int DIRTY = 1;
 
   private BuildConstants() {}
