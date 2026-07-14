39e375a18a09dc3b1bf1db189079b4dababa9e0a
Programming Laptop 2 <aztechs157General@gmail.com>
2026-07-14 19:11:48 -0400

    config and model for ascope - RN

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -5,12 +5,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "Offseason2026-1";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 5;
-  public static final String GIT_SHA = "eabb3e55313ca288c648bdeb570801f6b9d53f76";
-  public static final String GIT_DATE = "2026-05-01 20:19:09 EDT";
+  public static final int GIT_REVISION = 6;
+  public static final String GIT_SHA = "8ab9056782c1b3b3e79741521e2b6928e39bd673";
+  public static final String GIT_DATE = "2026-05-19 20:15:54 EDT";
   public static final String GIT_BRANCH = "hopperRollerSubsystem";
-  public static final String BUILD_DATE = "2026-05-19 20:12:37 EDT";
-  public static final long BUILD_UNIX_TIME = 1779235957048L;
+  public static final String BUILD_DATE = "2026-07-14 19:10:07 EDT";
+  public static final long BUILD_UNIX_TIME = 1784070607276L;
   public static final int DIRTY = 1;
 
   private BuildConstants() {}
